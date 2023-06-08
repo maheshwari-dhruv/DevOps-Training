3ebedbe97b20b25fbca1fdafafb8bac11b52ff6e
Dhruv Maheshwari <mrdm026@gmail.com>
2023-06-08 13:35:37 +0530

    Learning Built-in Data types

---
+++ python-training/build-in data types/built-in-data-types.py
@@ -0,0 +1,33 @@
+# Built-in Data Types
+# Strings --
+print("Hello" + "World")
+print("Hello\tWorld")
+print("Hello\nWorld")
+print("Hello '|' World")
+print("Hello" * 4)
+print("Hello".find('e'))
+
+# Numbers --
+print(2 + 2)
+print(2 - 5)
+print(3 * 3)
+print(5 / 3)
+print(5 // 3)
+print(5 % 3)
+print(2 ** 3)
+print(str(3.3))
+
+# Variables --
+myName = "Dhruv"
+print(myName)
+myName += " Maheshwari"
+print(myName)
+
+# List --
+myList = [1, 2, 3, 4]
+print(myList)
+print(myList[1])
+print(myList[1:3])
+print(myList[::2])
+print(myList[::-1])
+print(len(myList))
